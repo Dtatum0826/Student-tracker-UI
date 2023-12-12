5733b868a0da7945556db09953d293fb94585628
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-12-12 14:38:59 -0600

    Reconfigured Class for new components.
    Added Route authentication and login and register pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Dashboard from './Components/Dashboard';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import { isAuthenticated } from './utils/AuthService';
+
+const PrivateRoute = ({ element: Element, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      element={isAuthenticated() ? <Element /> : <Navigate to="/login" />}
+    />
+  );
+};
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+   </BrowserRouter>
   );
 };
 
